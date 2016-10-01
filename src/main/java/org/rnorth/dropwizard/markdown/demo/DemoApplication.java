bc229564492856997683230287fc963841006dfd
Richard North <rich.north@gmail.com>
2016-10-01 20:11:52 +0100

    Add redirect from / to /docs/ for demo purposes

--- src/main/java/org/rnorth/dropwizard/markdown/demo/DemoApplication.java
+++ src/main/java/org/rnorth/dropwizard/markdown/demo/DemoApplication.java
@@ -8,9 +8,15 @@ import org.rnorth.dropwizard.markdown.MarkdownAssetsBundle;
 import org.rnorth.dropwizard.markdown.MarkdownAssetsConfiguration;
 import org.rnorth.dropwizard.markdown.MarkdownBundleConfiguration;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
 /**
  * Created by rnorth on 29/09/2016.
  */
+@Path("/")
 public class DemoApplication extends Application<DemoApplication.DemoConfiguration> {
 
     @Override
@@ -22,7 +28,8 @@ public class DemoApplication extends Application<DemoApplication.DemoConfigurati
 
     @Override
     public void run(DemoConfiguration configuration, Environment environment) throws Exception {
-        // Nothing needed here for demo purposes
+        // Nothing needed here for demo purposes, but redirect the root path to /docs/
+        environment.jersey().register(this);
     }
 
     public static void main(String[] args) throws Exception {
@@ -37,6 +44,11 @@ public class DemoApplication extends Application<DemoApplication.DemoConfigurati
         public MarkdownAssetsConfiguration getMarkdownAssetsConfiguration() {
             return assets;
         }
+    }
+
+    @GET
+    public Response redirectRootToDocs() {
+        return Response.temporaryRedirect(UriBuilder.fromPath("/docs/").build()).build();
     }
 }
 
